d011f18e66f9021df801f040e82c54bd3023106f
13750376 <hsiehhongsheng@gmail.com>
2025-10-09 17:20:59 +0800

    week05

---
+++ week05/week05-1.cpp
@@ -0,0 +1,13 @@
+//week05-1.cpp
+//LeetCode 學習計畫 Built-In Functions
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s); //
+        string word; // 字串的 word
+        //cout <<＂讀到了＂ << "\n"
+        while(ss >> word){
+    }
+    return word.length();
+    }
+};
